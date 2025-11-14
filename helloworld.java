c68c6fd97c9975c342807cb84e5f5526fdc5575f
rushikeshjadhav-dev <112641060+rushikeshjadhav-dev@users.noreply.github.com>
2025-11-15 02:23:52 +0530

    Update helloworld.java
    added new

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello batch41 team with  Chirag, Aniket , Sarang, Sairam");
       System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Santhosh , naveen, komal in making CI automated vikas teach devops CI proecss");
+      System.out.println("Webhook Test");
+
     }  
 } 
