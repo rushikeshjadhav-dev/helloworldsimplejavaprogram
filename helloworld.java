63e483564b43d7f49a3dba2d27c9ffeb26cdf5b6
rushikeshjadhav-dev <112641060+rushikeshjadhav-dev@users.noreply.github.com>
2025-11-15 02:29:37 +0530

    Update helloworld.java
    Testing webhook

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,8 @@ class Simple{
      System.out.println("Hello batch41 team with  Chirag, Aniket , Sarang, Sairam");
       System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Santhosh , naveen, komal in making CI automated vikas teach devops CI proecss");
       System.out.println("Webhook Test");
+      System.out.println("Testing webhook");
+
 
     }  
 } 
